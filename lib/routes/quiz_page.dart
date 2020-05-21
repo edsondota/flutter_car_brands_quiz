d5276e5ab37737baee76b9ec0f784ab63977312d
Edson Dota <edson.dota@arctouch.com>
2020-05-21 16:12:09 -0300

    Add layout base to quiz page

--- lib/routes/quiz_page.dart
+++ lib/routes/quiz_page.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_car_brands_quiz/components/primary_button.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class QuizPage extends StatelessWidget {
+class QuizPage extends StatefulWidget {
   static String routeName = 'quiz_page';
+
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  var options = [];
+  @override
+  void initState() {
+    super.initState();
+    options = [
+      {
+        'option': 'Japan',
+        'isCorrect': false,
+        'isSelected': false,
+      },
+      {
+        'option': 'Brazil',
+        'isCorrect': false,
+        'isSelected': false,
+      },
+      {
+        'option': 'France',
+        'isCorrect': false,
+        'isSelected': false,
+      },
+      {
+        'option': 'Deustach',
+        'isCorrect': false,
+        'isSelected': false,
+      },
+    ];
+  }
+
+  void updateOption(int index) {
+    if (!options[index]['isSelected']) {
+      options.forEach((element) {
+        element['isSelected'] = false;
+      });
+      setState(() {
+        options[index]['isSelected'] = true;
+      });
+    } else {
+      setState(() {
+        options[index]['isSelected'] = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Color(0xFFF2F2F2),
       body: SafeArea(
         child: Column(
           children: <Widget>[
@@ -26,6 +76,7 @@ class QuizPage extends StatelessWidget {
                       child: Text(
                         'This car brand is (or was) originally from which country?',
                         textAlign: TextAlign.center,
+                        style: GoogleFonts.squadaOne(fontSize: 20.0),
                       ),
                     ),
                   ],
@@ -39,6 +90,38 @@ class QuizPage extends StatelessWidget {
                     topLeft: Radius.circular(30.0),
                   ),
                   color: Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: options.asMap().entries.map((entry) {
+                          var item = entry.value;
+                          return FlatButton(
+                            onPressed: () => updateOption(entry.key),
+                            color: item['isSelected'] ? Colors.black12 : Colors.transparent,
+                            child: ListTile(
+                              leading: item['isSelected'] ? Icon(Icons.label) : Icon(Icons.label_outline),
+                              title: Text(item['option']),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: (options.any((item) => item['isSelected']))
+                          ? PrimaryButton(
+                              child: Text('Confirm'),
+                              onPressed: () {},
+                            )
+                          : SizedBox(
+                              height: 50.0,
+                            ),
+                    ),
+                  ],
                 ),
               ),
             ),
