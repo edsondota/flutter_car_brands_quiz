ba9af41fe25a74490b636aa572b74c9a12a1af55
Edson Dota <edson.dota@arctouch.com>
2020-05-21 10:42:55 -0300

    Format the GET STARTED button

--- lib/routes/start_page.dart
+++ lib/routes/start_page.dart
@@ -7,29 +7,40 @@ class StartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Container(
-                height: 180.0,
-                width: 180.0,
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.transparent,
-                  backgroundImage: AssetImage('assets/icon.png'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Container(
+                    height: 180.0,
+                    width: 180.0,
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: AssetImage('assets/icon.png'),
+                    ),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('Start the game'),
-            ),
-          ],
+              Container(
+                height: 50.0,
+                width: double.infinity,
+                decoration: BoxDecoration(color: Color(0XFF2D291E), borderRadius: BorderRadius.all(Radius.circular(9.0))),
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text('GET STARTED'),
+                  textColor: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              )
+            ],
+          ),
         ),
       ),
     );
